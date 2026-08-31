68f04e3b97d51ae05c93a857ab091bb66db03751
ysyx_24100022-Qin Junfu <qinjun_fu@qq.com>
2026-08-31 11:26:01 +0800

    Finish nvboard Double-pole switch.

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,35 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
+#include <nvboard.h>
 #include <verilated.h>
 #include <verilated_vcd_c.h>
 #include <Vtop.h>
+
+void nvboard_bind_all_pins(TOP_NAME *top);
 
 int main(int argc, char **argv) {
   VerilatedContext *contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
   contextp->traceEverOn(true);
-  Vtop *top = new Vtop{contextp};
+  TOP_NAME *top = new TOP_NAME{contextp};
+
+  nvboard_bind_all_pins(top);
+  nvboard_init();
 
   VerilatedVcdC *trace = new VerilatedVcdC;
   top->trace(trace, 99);
   trace->open("waveform.vcd");
 
-  constexpr vluint64_t kMaxCycles = 100;
-  while (!contextp->gotFinish() && contextp->time() < kMaxCycles) {
-    int a = rand() & 1;
-    int b = rand() & 1;
-    top->a = a;
-    top->b = b;
+  while (!contextp->gotFinish()) {
+    nvboard_update();
     top->eval();
     trace->dump(contextp->time());
-    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-    assert(top->f == (a ^ b));
+    if ((contextp->time() & 0x3ff) == 0) trace->flush();
     contextp->timeInc(1);
   }
 
   top->final();
   trace->close();
+  nvboard_quit();
   delete trace;
   delete top;
   delete contextp;
